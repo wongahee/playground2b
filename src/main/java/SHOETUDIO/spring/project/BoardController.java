d3ae2e39d482471b9b7798629025506e6639c2f8
gahee <rkgml284@gmail.com>
2021-06-13 14:19:00 +0900

    팀원의 board 기능

---
+++ src/main/java/SHOETUDIO/spring/project/BoardController.java
@@ -0,0 +1,23 @@
+package SHOETUDIO.spring.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/board/list")
+    public String list(){
+        return "board/list";
+    }
+
+    @GetMapping("/board/write")
+    public String write(){
+        return "board/write";
+    }
+
+    @GetMapping("/board/view")
+    public String view(){
+        return "board/view";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/board/write.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>board/write</title>
+</head>
+<body>
+<h1>board-write</h1>
+</body>
+</html>
